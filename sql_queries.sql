765057e2718c7b78090f5d9caef7bba55bb16680
nikolabibercic <nikolabibercic@gmail.com>
2020-11-21 20:09:13 +0100

    dodati upiti za kreiranje tabela users, roles i user_roles kao i par inserta

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,3 +1,29 @@
 CREATE DATABASE parfemi 
 CHARACTER SET utf8 
 COLLATE utf8_unicode_ci;
+
+create table users(
+	user_id int AUTO_INCREMENT PRIMARY KEY,
+	name varchar(100) character set utf8 not null,
+    email varchar(100) character set utf8 not null unique,
+    password varchar(100) character set utf8 not null
+)engine=myisam;
+
+CREATE TABLE roles(
+	role_id int AUTO_INCREMENT PRIMARY KEY,
+	name varchar(50) character set utf8 not null unique
+)engine=myisam;
+
+CREATE TABLE user_roles(
+	user_role_id int AUTO_INCREMENT PRIMARY KEY,
+	user_id int not null,
+	role_id int not null,
+	FOREIGN KEY (user_id) REFERENCES users(user_id),
+	FOREIGN KEY (role_id) REFERENCES roles(role_id)
+)engine=myisam;
+
+insert into roles values(null,'Admin');
+insert into roles values(null,'Bloger');
+
+insert into users values(null,'Nikola Bibercic','nikolabibercic@gmail.com','123');
+insert into user_roles values(null,1,1);
